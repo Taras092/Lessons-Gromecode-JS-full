3500fcc7febde9f30c50e595f81d7e65b556e4a6
taras <taraslychynets@icloud.com>
2021-11-25 14:22:14 +0200

    unit

--- Lessons21/task4/index.js
+++ Lessons21/task4/index.js
@@ -18,4 +18,3 @@ export function getGoal() {
   return elemGoal.outerHTML;
 }
 
-
--- Lessons21/task5/index.js
+++ Lessons21/task5/index.js
@@ -2,5 +2,3 @@ export function setTitle(text) {
   const newTitle = document.querySelector('.title');
   newTitle.textContent = text;
 }
-
-console.log(setTitle('Taras'));
