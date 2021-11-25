3bb63bd159ec12b2eb986989706c442c31cdd297
taras <taraslychynets@icloud.com>
2021-11-25 09:42:21 +0200

    unit

--- Lessons21/task3/index.js
+++ Lessons21/task3/index.js
@@ -1,14 +1,14 @@
- function getItemsList() {
+export function getItemsList() {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 }
 
- function getItemsArray() {
+export function getItemsArray() {
   const elementsArray = document.querySelectorAll('.tool');
   console.dir(Array.from(elementsArray).map);
   return Array.from(elementsArray).map;
 }
 
-console.log(getItemsList());
-console.log(getItemsArray());
+// console.log(getItemsList());
+// console.log(getItemsArray());
