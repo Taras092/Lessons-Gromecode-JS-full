b36c679779defa367c23b438e603b81d5c6f4d10
taras <taraslychynets@icloud.com>
2021-11-26 11:40:51 +0200

    unit parent elem

--- Lessons21/task13/index.js
+++ Lessons21/task13/index.js
@@ -1,4 +1,13 @@
-export function clearList() {
-  const categories = document.querySelector('.categories');
-  categories.innerHTML = '';
+function getSection(num) {
+  const span = document.querySelectorAll('span');
+  const arr = Array.from(span);
+  const elem = arr.find(el => {
+    const numb = el.dataset.number;
+if (numb ===  num) {
+  return el.parentElement;
 }
+  });
+  console.log(elem)
+}
+
+console.log(getSection('1'));
