618722ed42a36c3cce81a5fdb95c0d5622fcd419
taras <taraslychynets@icloud.com>
2021-11-26 11:35:31 +0200

    unit dataset

--- Lessons21/task12/index.js
+++ Lessons21/task12/index.js
@@ -1,7 +1,23 @@
 export function squaredNumbers() {
-  const elem = document.querySelector('.number');
-  const objLi = elem.dataset.number**2;
-  elem.setAttribute('data-squared-number', objLi);
+  const elem = document.querySelectorAll(".number");
+  const arr = Array.from(elem);
+  const squaredEl = arr.map((el) => {
+    const sqr = el.dataset.number ** 2;
+    console.log(sqr);
+    return el.setAttribute("data-squared-number", sqr);
+  });
+  return squaredEl;
 }
+squaredNumbers();
 
+//  function squaredNumbers1() {
+//   const elem = document.querySelector('.number');
+//   const elements = elem.map(el => 
+//     el.setAttribute('data-squared-number',  el.dataset.number**2));
+//     return elements;
+// }
 
+// console.log(squaredNumbers1());
+// const squaredArr = arr
+  // .map(elm => elm.setAttribute('data-squared-number', squaredEl.map(el => el)));
+  // console.log(squaredArr);
