c3fae399de385f1da07f57a9a5fb0401454c89d7
taras <taraslychynets@icloud.com>
2021-11-29 14:36:45 +0200

    unit click textContent

---
+++ Lessons22/task5/index.js
@@ -0,0 +1,6 @@
+const buttonElem = document.querySelector(".search__btn");
+
+buttonElem.addEventListener("click", () => {
+  const inputElem = document.querySelector(".search__input");
+  console.log(inputElem.textContent);
+});
