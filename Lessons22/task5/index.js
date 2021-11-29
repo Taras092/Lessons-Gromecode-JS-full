a40e1d5a75f123923e85e9362ac89e84c7d2dfd1
taras <taraslychynets@icloud.com>
2021-11-29 14:38:48 +0200

    unit click inputElem.value

--- Lessons22/task5/index.js
+++ Lessons22/task5/index.js
@@ -2,5 +2,5 @@ const buttonElem = document.querySelector(".search__btn");
 
 buttonElem.addEventListener("click", () => {
   const inputElem = document.querySelector(".search__input");
-  console.log(inputElem.textContent);
+  console.log(inputElem.value);
 });
