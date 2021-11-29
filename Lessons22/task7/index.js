dad12f4caca953bfc5dfda8824b563c6959f640a
taras <taraslychynets@icloud.com>
2021-11-29 15:27:50 +0200

    unit click querySelectorAll and map

---
+++ Lessons22/task7/index.js
@@ -0,0 +1,9 @@
+const btnElements = document.querySelectorAll('.btn');
+const arr = Array.from(btnElements);
+
+const handleClick = (event) => {
+  console.log(event.target.textContent);
+}
+
+
+arr.map(el => el.addEventListener('click', handleClick));
