04b9e36f78b57336e6f11bccfc9586f7662812e0
taras <taraslychynets@icloud.com>
2021-12-02 13:36:15 +0200

    add diff dates

--- Lessons24/task3/index.js
+++ Lessons24/task3/index.js
@@ -3,9 +3,9 @@ export const getDiff = (startDate, endDate) => {
   const end = new Date(endDate);
   const diff = new Date(end.getTime() - start.getTime());
   const day = Math.abs(parseInt(diff / (24*3600*1000)));
-  const hour = Math.abs(Math.floor((diff / (1000 * 60 * 60)) % 24));
-  const minute = Math.abs(Math.floor((diff / (1000 * 60)) % 60));
-  const seconds = Math.abs(Math.floor((diff / 1000) % 60));
+  const hour = Math.abs(Math.floor((diff / (1000 * 60 * 60)) % 24)) - 1;
+  const minute = Math.abs(Math.floor((diff / (1000 * 60)) % 60)) - 1;
+  const seconds = Math.abs(Math.floor((diff / 1000) % 60)) -1;
   const result = `${day}d ${hour}h ${minute}m ${seconds}s`;
   return result;
 }
