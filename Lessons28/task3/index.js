84d4b16fda0d84c218cabdae1b3fc0306d153c4b
taras <taraslychynets@icloud.com>
2021-12-07 14:24:39 +0200

    Каррированиє

---
+++ Lessons28/task3/index.js
@@ -0,0 +1,17 @@
+// Карированиє
+
+// const sum = (a, b) => a + b;
+
+// const sum1 = a => b => a + b;
+
+// const add3 = sum(3);
+
+// const result = add3(5);
+// console.log(result);
+
+export const mult = firstValue => secondValue => firstValue * secondValue;
+
+export const twice = mult(2);
+
+export const triple = mult(3);
+
