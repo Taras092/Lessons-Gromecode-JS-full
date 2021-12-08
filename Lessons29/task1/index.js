86846424fa0ca8fd1311ddc18eb912a024925d2c
taras <taraslychynets@icloud.com>
2021-12-08 14:28:16 +0200

    load image

--- Lessons29/task1/index.js
+++ Lessons29/task1/index.js
@@ -30,8 +30,8 @@ const onImageLoaded = (error, imgElem) => {
 };
 
 // examples
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
 
