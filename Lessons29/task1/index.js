a3cf0cbbcc25420892314b3393d9d3cd59fdb079
taras <taraslychynets@icloud.com>
2021-12-08 14:55:15 +0200

    load image

--- Lessons29/task1/index.js
+++ Lessons29/task1/index.js
@@ -6,8 +6,7 @@ export const addImage = (imgSrc, callback) => {
   containerElem.append(imgElem);
 
   const onImageLoaded = () => {
-    const { width, height } = imgElem;
-    callback(null, { width, height });
+    callback(null, imgElem);
   };
 
   imgElem.addEventListener('load', onImageLoaded);
@@ -30,8 +29,8 @@ const onImageLoaded = (error, imgElem) => {
 };
 
 // examples
-// addImage(
-//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//   onImageLoaded,
-// );
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
 
