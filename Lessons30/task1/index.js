156bca379fffd9923b3d06294b6690cad1964772
taras <taraslychynets@icloud.com>
2021-12-09 02:57:25 +0200

    promise resolve snd reject

---
+++ Lessons30/task1/index.js
@@ -0,0 +1,33 @@
+export const addImage = (imgSrc) => {
+  const p = new Promise((resolveCb, rejectCb) => {
+    const imgElem = document.createElement("img");
+    imgElem.setAttribute("alt", "My Photo");
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector(".page");
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem.setAttribute("style", "width: 200; height: 100");
+      resolveCb({ width, height });
+    };
+
+    imgElem.addEventListener("load", onImageLoaded);
+
+    imgElem.addEventListener("error", () =>
+      rejectCb(new Error("Image load is failed"))
+    );
+  });
+
+  return p;
+};
+
+const imgSrc =
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
+
+const resultPromise = addImage(imgSrc);
+
+resultPromise.catch((error) => console.log(error));
+
+// examples
+// addImageWithPromise(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg');
