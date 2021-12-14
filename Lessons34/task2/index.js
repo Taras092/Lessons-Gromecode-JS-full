ff2c9c2cc0d0278ebce86bc9a5014f0d02bf9e8f
taras <taraslychynets@icloud.com>
2021-12-14 23:51:26 +0200

    CRUD Запросы на сервер

--- Lessons34/task2/index.js
+++ Lessons34/task2/index.js
@@ -1,4 +1,4 @@
-// const baseUrl = "https://61b8921e64e4a10017d19082.mockapi.io/api/v1/users";
+const baseUrl = "https://61b8921e64e4a10017d19082.mockapi.io/api/v1/users";
 
 function getUsersList() {
   return fetch(baseUrl).then((resolve) => resolve.json());
@@ -43,28 +43,28 @@ export function updateUser(userId, userData) {
 //   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
 // });
 
-// const newUserData = {
-//   email: "cool@email.com",
-//   firstName: "Iron",
-//   lastName: "Man",
-//   age: 42,
-// };
+const newUserData = {
+  email: "cool@email.com",
+  firstName: "Iron",
+  lastName: "Man",
+  age: 42,
+};
 
 // createUser(newUserData).then(() => {
 //   console.log("User created");
 // });
 
-// const updatedUserData = {
-//   email: "new@email.com",
-//   firstName: "John",
-//   lastName: "Doe",
-//   age: 17,
-// };
+const updatedUserData = {
+  email: "new@email.com",
+  firstName: "John",
+  lastName: "Doe",
+  age: 17,
+};
 
-// updateUser("22", updatedUserData).then(() => {
+// updateUser("1", updatedUserData).then(() => {
 //   console.log("User updated");
 // });
 
-// deleteUser("3").then(() => {
+// deleteUser("2").then(() => {
 //   console.log("User updated");
 // });
