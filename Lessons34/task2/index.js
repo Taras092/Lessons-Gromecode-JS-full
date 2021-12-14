1838c92a05b92b4136afa8cd3de57a71f186d004
taras <taraslychynets@icloud.com>
2021-12-14 23:50:01 +0200

    CRUD Запросы на сервер

--- Lessons34/task2/index.js
+++ Lessons34/task2/index.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://61b8921e64e4a10017d19082.mockapi.io/api/v1/users";
+// const baseUrl = "https://61b8921e64e4a10017d19082.mockapi.io/api/v1/users";
 
 function getUsersList() {
   return fetch(baseUrl).then((resolve) => resolve.json());
@@ -35,36 +35,36 @@ export function updateUser(userId, userData) {
 }
 
 // examples
-getUsersList().then((users) => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
+// getUsersList().then((users) => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
 
-getUserById("5").then((userData) => {
-  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-});
+// getUserById("5").then((userData) => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
 
-const newUserData = {
-  email: "cool@email.com",
-  firstName: "Iron",
-  lastName: "Man",
-  age: 42,
-};
+// const newUserData = {
+//   email: "cool@email.com",
+//   firstName: "Iron",
+//   lastName: "Man",
+//   age: 42,
+// };
 
-createUser(newUserData).then(() => {
-  console.log("User created");
-});
+// createUser(newUserData).then(() => {
+//   console.log("User created");
+// });
 
-const updatedUserData = {
-  email: "new@email.com",
-  firstName: "John",
-  lastName: "Doe",
-  age: 17,
-};
+// const updatedUserData = {
+//   email: "new@email.com",
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 17,
+// };
 
-updateUser("22", updatedUserData).then(() => {
-  console.log("User updated");
-});
+// updateUser("22", updatedUserData).then(() => {
+//   console.log("User updated");
+// });
 
-deleteUser("3").then(() => {
-  console.log("User updated");
-});
+// deleteUser("3").then(() => {
+//   console.log("User updated");
+// });
