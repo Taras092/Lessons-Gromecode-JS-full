766075200aca7402b6b21d58627a07660c163ebb
taras <taraslychynets@icloud.com>
2021-12-15 14:09:20 +0200

    обработчик  ошибок Promise

--- Lessosn35/task2/index.js
+++ Lessosn35/task2/index.js
@@ -55,3 +55,17 @@ failRequest
 // window.addEventListener('unhandledrejection', function (err) {
 //   console.log(err.reason.message);
 // });
+
+
+// fetch('https://api.github.com/users/github')
+//     .then(response => {
+//       if (response.status === 200) {
+//         return response.json();
+//       }
+//       throw new Error('No data');
+//     })
+//     .then(data => console.log(data))
+//     .catch(err => {
+//       debugger;
+//       console.log(err);
+//     });
